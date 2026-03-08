8ca7b514d3db8fe50558b602402899f7683929eb
vaibhavvahbiav <vaibhavatkurwar31@gmail.com>
2026-03-08 22:22:20 +0530

    Stress test for Linux systems

---
+++ stress_monitor.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Starting CPU stress test..."
+echo "Press Ctrl + C to stop."
+echo ""
+
+stress -c $(nproc) &
+STRESS_PID=$!
+
+while true 
+do 
+	clear
+
+	TEMP=$(cat /sys/class/thermal/thermal_zone0/temp 2>/dev/null)
+	TEMP_C=$(echo "$TEMP/1000" | bc -l)
+
+	echo "=== RAdxA Stress test ==="
+	echo ""
+	echo "CPU cores stressed : $(nproc)"
+	echo "Temperature : ${TEMP_C} 'C"
+	echo ""
+	echo "LOad average:"
+	uptime
+	echo ""
+	echo "CPU usage:"
+	top -bn1 | grep "Cpu(s)"
+	echo ""
+	echo "Memoery:"
+	free -h
+	echo ""
+	echo "made by Vaibhav"
+
+	sleep 1
+done
+
+kill $STRESS_PID
+
